bed867e20ab54f125586a418533c412fda7cb29d
Udimberto S. Junior <elvitous@gmail.com>
2016-10-03 01:32:45 -0300

    update app interface to materialize

--- app/scripts/directives/navbar-directive.js
+++ app/scripts/directives/navbar-directive.js
@@ -1,49 +1,16 @@
 'use strict';
 
 angular.module('rastros')
-.directive('navbar', function ($window, flow, user, authentication) {
+.directive('navbar', function ($window, flow, user) {
 	return {
 		restrict   : 'E',
 		scope      : true,
 		templateUrl: 'app/templates/directives/navbar-directive-template.html',
 		link       : function (scope) {
-			scope.screen = $window.innerWidth;
-			scope.opened = true;
-
-			scope.logout = function () {
-				authentication.logout();
-			};
-
 			scope.goTo = function (page) {
 				var _page = page || '/';
 				flow.goTo(_page);
 			};
-
-			scope.toggle = function () {
-				scope.opened = !scope.opened;
-			};
-
-			scope.handle = function () {
-				if (scope.screen < 768) {
-					scope.opened = false;
-					return;
-				}
-				
-				scope.opened = true;
-			};
-
-			scope.resized = function () {
-				if (scope.screen !== $window.innerWidth) {
-					scope.screen = $window.innerWidth;
-
-					scope.handle();
-				}
-			};
-
-			scope.handle();
-
-			angular.element($window)
-				.bind('resize', scope.resized);
 		},
 		controller : function ($scope) {
 			$scope.user = user.data ? user.data : user.isLogged();
--- tasks/copy.js
+++ tasks/copy.js
@@ -5,17 +5,12 @@ const gulp  = require('gulp'),
       SRC   = [
         './app/assets/site.css',
         './app/manifest.json',
-      ],
-      fonts = './app/bower_components/font-awesome/fonts/**/*.{ttf,woff,woff2,eof,svg}';
+      ];
 
 /**
  * Copy main files to dist folder
  */
 gulp.task('copy', ['usemin', 'clean:dist'], () => {
-    gulp.src(fonts)
-        .pipe(gulp.dest(paths.dist + '/fonts'))
-        .pipe(gulp.dest(paths.docs + '/fonts'));
-
     const stream = gulp.src(SRC, { base: './app/' })
         .pipe(gulp.dest(paths.dist))
         .pipe(gulp.dest(paths.docs));
